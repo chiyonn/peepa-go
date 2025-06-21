574064d4a0ba1334ae4f4a14cae6a08ed624fe3a
chiyonn <kud0h.chi4@gmail.com>
2025-06-21 11:37:50 +0900

    update: formatted

--- internal/client/response.go
+++ internal/client/response.go
@@ -6,49 +6,49 @@ type AuthResponse struct {
 	} `json:"AuthenticationResult"`
 }
 
-type rawProductDetailResponse  struct {
+type rawProductDetailResponse struct {
 	Data struct {
 		GetProductDetail MetaData `json:"getProductDetail"`
 	} `json:"data"`
 }
 
 type MetaData struct {
-	ASIN		  string `json:"asin"`
+	ASIN      string `json:"asin"`
 	CreatedAt string `json:"createdAt"`
 	JSON      string `json:"json"`
 }
 
 type RawProduct struct {
-	ASIN string `json:"asin"`
-	Title string `json:"title"`
-	RootCategory int64 `json:"rootCategory"`
-	Categories []int64 `json:"categories"`
-	ImagesCSV string `json:"imagesCSV"`
-	Brand string `json:"brand"`
-	Manifacturer string `json:"manifacturer"`
-	LastPriceChange int64 `json:"lastPriceChange"`
-	LastUpdate int64 `json:"lastUpdated"`
-	Offers []RawOffer `json:"offers"`
+	ASIN            string     `json:"asin"`
+	Title           string     `json:"title"`
+	RootCategory    int64      `json:"rootCategory"`
+	Categories      []int64    `json:"categories"`
+	ImagesCSV       string     `json:"imagesCSV"`
+	Brand           string     `json:"brand"`
+	Manifacturer    string     `json:"manifacturer"`
+	LastPriceChange int64      `json:"lastPriceChange"`
+	LastUpdate      int64      `json:"lastUpdated"`
+	Offers          []RawOffer `json:"offers"`
 }
 
 type RawOffer struct {
-	LastSeen         int      `json:"lastSeen"`
-	SellerID         string   `json:"sellerId"`
-	OfferCSV         []int    `json:"offerCSV"`
-	Condition        int      `json:"condition"`
-	IsPrime          bool     `json:"isPrime"`
-	IsMAP            bool     `json:"isMAP"`
-	IsShippable      bool     `json:"isShippable"`
-	IsAddonItem      bool     `json:"isAddonItem"`
-	IsPreorder       bool     `json:"isPreorder"`
-	IsWarehouseDeal  bool     `json:"isWarehouseDeal"`
-	IsScam           bool     `json:"isScam"`
-	IsAmazon         bool     `json:"isAmazon"`
-	IsPrimeExcl      bool     `json:"isPrimeExcl"`
-	OfferID          int      `json:"offerId"`
-	IsFBA            bool     `json:"isFBA"`
-	ShipsFromChina   bool     `json:"shipsFromChina"`
-	MinOrderQty      int      `json:"minOrderQty"`
-	CouponHistory    []int    `json:"couponHistory"`
-	LastStockUpdate  int      `json:"lastStockUpdate"`
+	LastSeen        int    `json:"lastSeen"`
+	SellerID        string `json:"sellerId"`
+	OfferCSV        []int  `json:"offerCSV"`
+	Condition       int    `json:"condition"`
+	IsPrime         bool   `json:"isPrime"`
+	IsMAP           bool   `json:"isMAP"`
+	IsShippable     bool   `json:"isShippable"`
+	IsAddonItem     bool   `json:"isAddonItem"`
+	IsPreorder      bool   `json:"isPreorder"`
+	IsWarehouseDeal bool   `json:"isWarehouseDeal"`
+	IsScam          bool   `json:"isScam"`
+	IsAmazon        bool   `json:"isAmazon"`
+	IsPrimeExcl     bool   `json:"isPrimeExcl"`
+	OfferID         int    `json:"offerId"`
+	IsFBA           bool   `json:"isFBA"`
+	ShipsFromChina  bool   `json:"shipsFromChina"`
+	MinOrderQty     int    `json:"minOrderQty"`
+	CouponHistory   []int  `json:"couponHistory"`
+	LastStockUpdate int    `json:"lastStockUpdate"`
 }
